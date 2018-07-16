1d75828e4af332a38818e3d2b747cd7bc8171279
Tomáš Oplatek <tomas.oplatek@gmail.com>
2018-07-16 22:54:39 +0200

    test

---
+++ TVS_Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace TVS_Server
+{
+    class Program
+    {
+        public static bool GUIEnabeled { get; set; } = false;
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().LogToDebug();
+
+        static void Main(string[] args) {
+            ParseParameters(args);
+            LoadApplication();
+            StartApplication();
+            Console.ReadLine();
+        }
+
+        private static void ParseParameters(string[] args) {
+
+        }
+
+        private static void LoadApplication() {
+
+        }
+
+        private static void StartApplication() {
+            if (GUIEnabeled) {
+                BuildAvaloniaApp().Start<MainWindow>();
+            } else {
+                TestMethod();
+            }
+        }
+
+        private static async void TestMethod() {
+            Log.Write(DateTime.Now.ToShortDateString()+ ", " + DateTime.Now.ToLongTimeString()+", " + Helper.GetMyIP());
+            await Database.LoadDatabase();
+            await Database.CreateDatabase(121361);
+            await Task.Delay(0);
+        }
+
+
+    }
+}
